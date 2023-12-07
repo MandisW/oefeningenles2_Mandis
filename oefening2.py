5dc559e67443409adb8add25c702c3aef71f2b2e
Mandis <mandiswatteny@msn.com>
2023-12-07 20:25:46 +0100

    oefening2

---
+++ oefening2.py
@@ -0,0 +1,8 @@
+getal_1 = int(input("geef een getal in: "))
+getal_2 = int(input("geef een tweede getal in: "))
+
+getal_3 = (getal_1 + getal_2)
+getal_4 = (getal_1 * getal_2)
+
+print (getal_3, "is de som van de 2 getallen! ")
+print (getal_4, "is het product van de 2 getallen! ")
