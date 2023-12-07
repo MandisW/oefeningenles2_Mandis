53cfb1a90784cbb3ce625bec505b6f511e9db562
Mandis <mandiswatteny@msn.com>
2023-12-07 20:27:55 +0100

    versie met +4 in de som

--- oefening2.py
+++ oefening2.py
@@ -1,8 +1,8 @@
 getal_1 = int(input("geef een getal in: "))
 getal_2 = int(input("geef een tweede getal in: "))
 
-getal_3 = (getal_1 + getal_2)
+getal_3 = (getal_1 + getal_2 +4)
 getal_4 = (getal_1 * getal_2)
 
-print (getal_3, "is de som van de 2 getallen! ")
+print (getal_3, "is de som van de 2 getallen +4! ")
 print (getal_4, "is het product van de 2 getallen! ")
